b8e6b21ced3a07bd44bd2b6ea651c45604eddf09
Bruno Silva <brunosilvinosilva@gmail.com>
2020-01-07 14:02:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -56,7 +56,7 @@ def map_to_no_change(source_array)
    counter = 0 
    while counter < source_array.size do 
      total += source_array[counter] 
-     counter += 1
+     counter += 1                                   
    end 
    total 
  end 
